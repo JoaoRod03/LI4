0b5cbd59afffa04c68567be7291657ff6fbfa183
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-01-21 22:41:28 +0000

    Ir buscar à database

--- LIForCars/Pages/Auction.cshtml.cs
+++ LIForCars/Pages/Auction.cshtml.cs
@@ -1,12 +1,29 @@
+using System.Collections.Generic;
+using LIForCars.Data.Interfaces;
+using LIForCars.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class AuctionModel : PageModel
 {
+    public Auction Auction { get; set; }
+
+    public DateTime futureDateTime;
+
     public IActionResult OnGetHeaderPartial() => Partial("Shared/Header");
     public IActionResult OnGetFooterPartial() => Partial("Shared/Footer");
     public IActionResult OnGetAboutPartial() => Partial("Shared/About");
     public IActionResult OnGetContactPartial() => Partial("Shared/Contact");
     public IActionResult OnGetLoginPartial() => Partial("Shared/Login");
     public IActionResult OnGetRegisterPartial() => Partial("Shared/Register");
+
+    public void OnGet()
+    {
+        // Initialize or retrieve data for the auction
+        Auction = new Auction();
+        futureDateTime = Auction.EndDateTime;
+    }
+
+
+
 }
